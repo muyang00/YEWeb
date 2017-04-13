e17f63389f1b99be7b59ba7d751b38135188e57a
yye <280798744@qq.com>
2017-04-13 17:18:20 +0800

    JS学习

---
+++ YEJS/myScript.js
@@ -0,0 +1,13 @@
+
+// 外部脚本不能包含 <script> 标签。
+function myFunction()
+{
+	var x=document.getElementById("demo").value;
+	if(x==""||isNaN(x))
+	{
+		alert("不是数字");
+	}
+	if (x==""||isNaN(x)) {
+		alert("caca");
+	};
+}
